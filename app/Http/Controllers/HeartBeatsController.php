e0220976e27d2bae06364e985d4d33c1f046abd7
williammabotjadev <williammabotjadev@gmail.com>
2021-11-23 18:11:29 +0200

    HeartBeats Controller, Home Controller

--- app/Http/Controllers/HeartBeatsController.php
+++ app/Http/Controllers/HeartBeatsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\HeartBeat;
 
 class HeartBeatsController extends Controller
 {
@@ -13,6 +14,24 @@ class HeartBeatsController extends Controller
 
     public function store(Request $request)
     {
+        $heartbeat_data = [
+            'device' => $request['device'],
+            'time' => $request['time'],
+            'snr' => $request['snr'],
+            'station' => $request['station'],
+            'data' => $request['data'],
+            'avgSnr' => $request['avgSnr'],
+            'lat' => $request['lat'],
+            'lng' => $request['lng'],
+            'rssi' => $request['rssi'],
+            'seqNumber' => $request['seqNumber']
+        ];
 
+        $heartbeat = HeartBeat::create($heartbeat_data);
+        $heartbeat->save();
+
+        $heartbeats = HeartBeat::all();
+
+        return view('home', ['heartbeats' => $heartbeats]);
     }
 }
