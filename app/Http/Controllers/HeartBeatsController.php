b0f1fc29ab9a5416f5e594cdf2b75706ff161609
williammabotjadev <williammabotjadev@gmail.com>
2021-11-23 18:05:15 +0200

    heartbeats controller

--- app/Http/Controllers/HeartBeatsController.php
+++ app/Http/Controllers/HeartBeatsController.php
@@ -6,5 +6,13 @@ use Illuminate\Http\Request;
 
 class HeartBeatsController extends Controller
 {
-    //
+    public function index()
+    {
+
+    }
+
+    public function store(Request $request)
+    {
+
+    }
 }
