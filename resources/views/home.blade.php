828c63227cf1c21bfebbec2a59afaf78e3759c16
williammabotjadev <williammabotjadev@gmail.com>
2021-11-23 18:37:36 +0200

    table format, style

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,20 +16,32 @@
 
                     <h3 style="text-align: center">{{ __('Latest HeartBeats') }}</h3>
                     <br>
-                    <table>
+                    <table class="table">
+                    <tr>
+                        <th class="px-2">Device No.</th>
+                        <th class="px-2">Time</th>
+                        <th class="px-2">Snr</th>
+                        <th class="px-2">Station</th>
+                        <th class="px-2">Data</th>
+                        <th class="px-2">Avg. Snr</th>
+                        <th class="px-2">Latitude</th>
+                        <th class="px-2">Longitude</th>
+                        <th class="px-2">RSSI</th>
+                        <th class="px-2">Seq. No</th>
+                    </tr>
                     @if (count($heartbeats) > 0)
                     @foreach($heartbeats as $heartbeat)
                         <tr>
-                            <td class="px-5"><h6><strong>{{ $heartbeat->device }}</strong></h6></td>
-                            <td class="px-5"><h6><strong>{{ $heartbeat->time }}</strong></h6></td>
-                            <td class="px-5"><h6><strong>{{ $heartbeat->snr }}</strong></h6></td>
-                            <td class="px-5"><h6><strong>{{ $heartbeat->station }}</strong></h6></td>
-                            <td class="px-5"><h6><strong>{{ $heartbeat->data }}</strong></h6></td>
-                            <td class="px-5"><h6><strong>{{ $heartbeat->avgSnr }}</strong></h6></td>
-                            <td class="px-5"><h6><strong>{{ $heartbeat->lat }}</strong></h6></td>
-                            <td class="px-5"><h6><strong>{{ $heartbeat->lng }}</strong></h6></td>
-                            <td class="px-5"><h6><strong>{{ $heartbeat->rssi }}</strong></h6></td>
-                            <td class="px-5"><h6><strong>{{ $heartbeat->seqNumber }}</strong></h6></td>
+                            <td class="px-2"><h6><strong>{{ $heartbeat->device }}</strong></h6></td>
+                            <td class="px-2"><h6><strong>{{ $heartbeat->time }}</strong></h6></td>
+                            <td class="px-2"><h6><strong>{{ $heartbeat->snr }}</strong></h6></td>
+                            <td class="px-2"><h6><strong>{{ $heartbeat->station }}</strong></h6></td>
+                            <td class="px-2"><h6><strong>{{ $heartbeat->data }}</strong></h6></td>
+                            <td class="px-2"><h6><strong>{{ $heartbeat->avgSnr }}</strong></h6></td>
+                            <td class="px-2"><h6><strong>{{ $heartbeat->lat }}</strong></h6></td>
+                            <td class="px-2"><h6><strong>{{ $heartbeat->lng }}</strong></h6></td>
+                            <td class="px-2"><h6><strong>{{ $heartbeat->rssi }}</strong></h6></td>
+                            <td class="px-2"><h6><strong>{{ $heartbeat->seqNumber }}</strong></h6></td>
                         </tr>
                 @endforeach
             </table>
