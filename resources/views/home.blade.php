a6a77f75ee6c284c5039cf8e5e98c902945af5ab
williammabotjadev <williammabotjadev@gmail.com>
2021-11-23 18:26:49 +0200

    heartbeat listing

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,7 +16,18 @@
 
                     <h3 style="text-align: center">{{ __('Latest HeartBeats') }}</h3>
                     <br>
-                    {{ $heartbeats }}
+                    <table>
+                    @if (count($heartbeats) > 0)
+                    @foreach($heartbeats as $heartbeat)
+                        <tr>
+                            <td class="px-5"><h6><strong>{{ $heartbeat->device }}</strong></h6></td>
+                            <td class="px-5"><h6><strong>{{ $heartbeat->time }}</strong></h6></td>
+                        </tr>
+                @endforeach
+            </table>
+            @else 
+                <h3 class="mt-5 mb-5">No HeartBeats Data Yet</h3>
+            @endif
                 </div>
             </div>
         </div>
